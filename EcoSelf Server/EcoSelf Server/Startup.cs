25c805cf66a77f8d25e18d8c5dc857fce738eec2
Egor <egor.popov42@gmail.com>
2020-07-11 20:56:26 +0300

    Added routing and MVC NuGet
    Андрей, если у тебя ошибка, то скачать нагетс мвс от майкрософта

--- EcoSelf Server/EcoSelf Server/Startup.cs
+++ EcoSelf Server/EcoSelf Server/Startup.cs
@@ -6,7 +6,6 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 
 namespace EcoSelf_Server
 {
@@ -16,25 +15,24 @@ namespace EcoSelf_Server
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            app.UseDeveloperExceptionPage();
+            app.UseStatusCodePages();
 
             app.UseRouting();
-
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Scanner}/{action=Index}/{id?}");
             });
+            
+
         }
     }
 }
